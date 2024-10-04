76ef960b810824a7345ddaeab9fe11794a1f1873
camilogs <camilo.gonzalez.col@gmail.com>
2024-10-04 09:51:22 -0500

    Add Platform.Type field

--- GameStore/GameStore.Core/Models/Platform.cs
+++ GameStore/GameStore.Core/Models/Platform.cs
@@ -3,4 +3,6 @@ namespace GameStore.Core.Models;
 public class Platform
 {
     public int Id { get; set; }
+
+    public string Type { get; set; }
 }
--- GameStore/GameStore.Tests/Seed/PlatformSeed.cs
+++ GameStore/GameStore.Tests/Seed/PlatformSeed.cs
@@ -6,6 +6,9 @@ public static class PlatformSeed
 {
     public static List<Platform> Platforms =>
         [
-            new() { Id = 1 },
+            new() { Id = 1, Type = "Mobile" },
+            new() { Id = 2, Type = "Browser" },
+            new() { Id = 3, Type = "Desktop" },
+            new() { Id = 4, Type = "Console" },
         ];
 }
