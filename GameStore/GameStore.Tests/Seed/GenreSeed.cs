cc9fdfee01a203958b50a92d7185bc42c3c278e0
camilogs <camilo.gonzalez.col@gmail.com>
2024-10-13 16:45:17 -0500

    extract static properties in seeders

--- GameStore/GameStore.Tests/Seed/GenreSeed.cs
+++ GameStore/GameStore.Tests/Seed/GenreSeed.cs
@@ -4,21 +4,25 @@ namespace GameStore.Tests.Seed;
 
 public class GenreSeed
 {
+    public static Genre Action => new()
+    {
+        Id = Guid.Parse("d55a9838-ddfa-4e90-bc24-463c2ad969f8"),
+        Name = "Action",
+    };
+
+    public static Genre Shooter => new()
+    {
+        Id = Guid.Parse("6589b697-fd37-4db8-86a6-9e7b5e7fab36"),
+        Name = "Shooter",
+        ParentGenreId = Action.Id,
+    };
+
     public static List<Genre> GetGenres()
     {
         return
         [
-            new()
-            {
-                Id = Guid.Parse("d55a9838-ddfa-4e90-bc24-463c2ad969f8"),
-                Name = "Action",
-            },
-            new()
-            {
-                Id = Guid.Parse("6589b697-fd37-4db8-86a6-9e7b5e7fab36"),
-                Name = "Shooter",
-                ParentGenreId = Guid.Parse("d55a9838-ddfa-4e90-bc24-463c2ad969f8"),
-            },
+            Action,
+            Shooter,
         ];
     }
 }
--- GameStore/GameStore.Tests/Seed/PlatformSeed.cs
+++ GameStore/GameStore.Tests/Seed/PlatformSeed.cs
@@ -4,14 +4,22 @@ namespace GameStore.Tests.Seed;
 
 public static class PlatformSeed
 {
+    public static Platform Mobile => new() { Id = Guid.Parse("89f83d50-34c7-4a3d-8dfc-77d5b89a39ab"), Type = "Mobile" };
+
+    public static Platform Browser => new() { Id = Guid.Parse("5e5c5ce6-ba25-4d2e-924c-ed14735dc2f8"), Type = "Browser" };
+
+    public static Platform Desktop => new() { Id = Guid.Parse("7455cf5c-4f7d-4f9c-8bbe-f6da2fea98eb"), Type = "Desktop" };
+
+    public static Platform Console => new() { Id = Guid.Parse("3d88e672-4b90-45e0-be2b-202673459273"), Type = "Console" };
+
     public static List<Platform> GetPlatforms()
     {
         return
         [
-            new() { Id = Guid.Parse("89f83d50-34c7-4a3d-8dfc-77d5b89a39ab"), Type = "Mobile" },
-            new() { Id = Guid.Parse("5e5c5ce6-ba25-4d2e-924c-ed14735dc2f8"), Type = "Browser" },
-            new() { Id = Guid.Parse("7455cf5c-4f7d-4f9c-8bbe-f6da2fea98eb"), Type = "Desktop" },
-            new() { Id = Guid.Parse("3d88e672-4b90-45e0-be2b-202673459273"), Type = "Console" },
+            Mobile,
+            Browser,
+            Desktop,
+            Console
         ];
     }
 }
