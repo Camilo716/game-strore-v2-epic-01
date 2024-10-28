1159b909cedffb39052046df48b9e6b85c134339
camilogs <camilo.gonzalez.col@gmail.com>
2024-10-28 14:05:20 -0500

    Add genres seeding

--- GameStore/GameStore.Infraestructure/Data/GameStoreDbContext.cs
+++ GameStore/GameStore.Infraestructure/Data/GameStoreDbContext.cs
@@ -13,6 +13,42 @@ public class GameStoreDbContext(DbContextOptions options) : DbContext(options)
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        SeedGenres(modelBuilder);
+        SeedPlatforms(modelBuilder);
+    }
+
+    private static void SeedGenres(ModelBuilder modelBuilder)
+    {
+        var strategyId = Guid.NewGuid();
+        var rpgId = Guid.NewGuid();
+        var sportsId = Guid.NewGuid();
+        var actionId = Guid.NewGuid();
+        var adventureId = Guid.NewGuid();
+        var puzzleSkillId = Guid.NewGuid();
+
+        // Seed main genres
+        modelBuilder.Entity<Genre>().HasData(
+            new Genre { Id = strategyId, Name = "Strategy" },
+            new Genre { Id = rpgId, Name = "RPG" },
+            new Genre { Id = sportsId, Name = "Sports" },
+            new Genre { Id = actionId, Name = "Action" },
+            new Genre { Id = adventureId, Name = "Adventure" },
+            new Genre { Id = puzzleSkillId, Name = "Puzzle & Skill" });
+
+        // Subgenres
+        modelBuilder.Entity<Genre>().HasData(
+            new Genre { Id = Guid.NewGuid(), Name = "RTS", ParentGenreId = strategyId },
+            new Genre { Id = Guid.NewGuid(), Name = "TBS", ParentGenreId = strategyId },
+            new Genre { Id = Guid.NewGuid(), Name = "Races", ParentGenreId = sportsId },
+            new Genre { Id = Guid.NewGuid(), Name = "Rally", ParentGenreId = sportsId },
+            new Genre { Id = Guid.NewGuid(), Name = "Formula", ParentGenreId = sportsId },
+            new Genre { Id = Guid.NewGuid(), Name = "Off-road", ParentGenreId = sportsId },
+            new Genre { Id = Guid.NewGuid(), Name = "FPS", ParentGenreId = actionId },
+            new Genre { Id = Guid.NewGuid(), Name = "TPS", ParentGenreId = actionId });
+    }
+
+    private static void SeedPlatforms(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Platform>().HasData([
             new() { Type = "Mobile" },
             new() { Type = "Browser" },
